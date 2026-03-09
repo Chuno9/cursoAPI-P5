0432b670b59664da9fe0b419e9f1f39111cb29a5
Manuel <manuelalvarez688@gmail.com>
2026-03-09 12:14:44 +0000

    Añadidos primeros endpoints

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends, HTTPException, Path
+from starlette import status
 import models
-from database import engine, Base
+from models import Todos
+from database import engine, SessionLocal
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def obtenerDB():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency=Annotated[Session, Depends(obtenerDB)]
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def leerDatos(db: db_dependency):
+    return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def datosPorId(db: db_dependency, todo_id: int=Path(gt=0)):
+    todo_model=db.query(Todos).filter(Todos.id==todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="No encontrado")
