c90c3727edd06ea2b78d3d9b4c50a4516cd4735b
Manuel <manuelalvarez688@gmail.com>
2026-03-09 14:10:01 +0000

    Todos los endpoints de la api funcionando

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Annotated
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, Path
 from starlette import status
+from pydantic import BaseModel, Field
 import models
 from models import Todos
 from database import engine, SessionLocal
@@ -18,6 +19,12 @@ def obtenerDB():
 
 db_dependency=Annotated[Session, Depends(obtenerDB)]
 
+class SolicitudTodo(BaseModel):
+    titulo:str=Field(min_length=3)
+    descripcion:str=Field(min_length=3, max_length=100)
+    prioridad:int=Field(gt=0, lt=6)
+    completada:bool
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def leerDatos(db: db_dependency):
     return db.query(Todos).all()
@@ -28,3 +35,30 @@ async def datosPorId(db: db_dependency, todo_id: int=Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail="No encontrado")
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def crearTodo(db:db_dependency, solicitud:SolicitudTodo):
+    todo_model=Todos(**solicitud.dict())
+
+    db.add(todo_model)
+    db.commit()
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def actualizarTodo(db: db_dependency, solicitud:SolicitudTodo, todo_id:int=Path(gt=0)):
+    todo_model=db.query(Todos).filter(Todos.id==todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, details="no se encuentra la tarea")
+    todo_model.titulo=solicitud.titulo
+    todo_model.descripcion=solicitud.descripcion
+    todo_model.prioridad=solicitud.prioridad
+    todo_model.completada=solicitud.completada
+    db.add(todo_model)
+    db.commit()
+    
+@app.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def borrarTodo(db: db_dependency, todo_id:int = Path(gt=0)):
+    todo_model=db.query(Todos).filter(Todos.id==todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="no se encuentra la tarea")
+    db.query(Todos).filter(Todos.id==todo_id).delete()
+    db.commit()
